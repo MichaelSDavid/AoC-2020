29a4081e5875328ca4a00f031bf5f238d3d0875b
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-12-02 17:13:21 -0500

    Add day2

---
+++ Day 2/day2.py
@@ -0,0 +1,23 @@
+import re
+    
+# Part 1
+count = 0
+
+with open('input.txt') as f:
+    for line in f:
+        mini, maxi, char, passwd = re.split('-| |: ', line)
+        if int(mini) <= passwd.count(char) <= int(maxi):
+            count += 1
+
+print(f"Pass rule one:\n{count}")
+
+# Part 2
+count = 0
+
+with open('input.txt') as f:
+    for line in f:
+        pos_a, pos_b, char, passwd = re.split('-| |: ', line)
+        if (char == passwd[int(pos_a)-1]) ^ (char ==  passwd[int(pos_b)-1]):
+            count += 1
+        
+print(f"Pass rule two:\n{count}")
