39286ef46fdf432aaa978f4a9311abae182fc986
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-12-02 17:14:21 -0500

    Update day2.py

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,11 +1,11 @@
-import re
+from re import split
     
 # Part 1
 count = 0
 
 with open('input.txt') as f:
     for line in f:
-        mini, maxi, char, passwd = re.split('-| |: ', line)
+        mini, maxi, char, passwd = split('-| |: ', line)
         if int(mini) <= passwd.count(char) <= int(maxi):
             count += 1
 
@@ -16,7 +16,7 @@ count = 0
 
 with open('input.txt') as f:
     for line in f:
-        pos_a, pos_b, char, passwd = re.split('-| |: ', line)
+        pos_a, pos_b, char, passwd = split('-| |: ', line)
         if (char == passwd[int(pos_a)-1]) ^ (char ==  passwd[int(pos_b)-1]):
             count += 1
         
