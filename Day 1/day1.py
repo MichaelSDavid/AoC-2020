cd79ec7e4d764a2550be87a23a3bb9e0595c64ec
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-12-01 20:10:02 -0500

    Update day1.py

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -8,13 +8,11 @@ group = combinations(content, 2)
 
 for pair in group:
     if int(pair[0]) + int(pair[1]) == 2020:
-        print("Pair:")
-        print(int(pair[0]) * int(pair[1]))
+        print(f"Pair:\n{int(pair[0]) * int(pair[1])}")
 
 # Part 2
 group = combinations(content, 3)
 
 for triplet in group:
     if int(triplet[0]) + int(triplet[1]) + int(triplet[2]) == 2020:
-        print("Triplet:")
-        print(int(triplet[0]) * int(triplet[1]) * int(triplet[2]))
+        print(f"Triplet:\n{int(triplet[0]) * int(triplet[1]) * int(triplet[2])}")
